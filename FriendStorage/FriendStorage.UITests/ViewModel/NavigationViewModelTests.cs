3be472ba39b1263c25f2fd0be74926302dc27d23
Sotnyk <serge.sotnyk@gmail.com>
2018-03-03 23:40:40 +0200

    After 36 - Mocking Dependencies with Moq - Use Moq in the NavigationViewModel-tests

--- FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
+++ FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
@@ -1,6 +1,7 @@
 ﻿using FriendStorage.Model;
 using FriendStorage.UI.DataProvider;
 using FriendStorage.UI.ViewModel;
+using Moq;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -9,10 +10,22 @@ namespace FriendStorage.UITests.ViewModel
 {
     public class NavigationViewModelTests
     {
+        INavigationDataProvider ConstructMock()
+        {
+            var navigationDataProviderMock = new Mock<INavigationDataProvider>();
+            navigationDataProviderMock.Setup(dp => dp.GetAllFriends())
+                .Returns(new List<LookupItem>
+                    {
+                        new LookupItem { Id = 1, DisplayMember = "Julia" },
+                        new LookupItem { Id = 2, DisplayMember = "Thomas" },
+                    });
+            return navigationDataProviderMock.Object;
+        }
+
         [Fact]
         public void ShouldLoadFriends()
         {
-            var viewModel = new NavigationViewModel(new NavigationDataProviderMock());
+            var viewModel = new NavigationViewModel(ConstructMock());
             viewModel.Load();
             Assert.Equal(2, viewModel.Friends.Count);
 
@@ -24,20 +37,11 @@ namespace FriendStorage.UITests.ViewModel
         [Fact]
         public void ShouldLoadFriendsOnlyOnce()
         {
-            var viewModel = new NavigationViewModel(new NavigationDataProviderMock());
+            var viewModel = new NavigationViewModel(ConstructMock());
             viewModel.Load();
             viewModel.Load();
             Assert.Equal(2, viewModel.Friends.Count);
 
-        }
-    }
-
-    public class NavigationDataProviderMock : INavigationDataProvider
-    {
-        public IEnumerable<LookupItem> GetAllFriends()
-        {
-            yield return new LookupItem { Id = 1, DisplayMember = "Julia" };
-            yield return new LookupItem { Id = 2, DisplayMember = "Thomas" };
         }
     }
 }
